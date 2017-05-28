110c0a534ab4c26c94051296f508be194be8e03a
girishramnani <girishramnani95@gmail.com>
2017-05-28 15:55:23 +0530

    string encoding completed with tests

--- lib/rlp/encode.ex
+++ lib/rlp/encode.ex
@@ -20,8 +20,23 @@ defmodule RLP.Encode do
         
         cond do
             (byte_size(payload) == 1) && Enum.member?(0x00..0x7f,byte) -> <<byte>>
-            byte_size(payload) < 56 ->  << @string + byte_size(payload) >> <> payload            
+            byte_size(payload) <= 55 ->  << @string + byte_size(payload) >> <> payload          
+            byte_size(payload) > 55 -> 
+                                    first_byte = int_to_binary(byte_size(payload))
+                                    <<@long_string + byte_size(first_byte) >> <> first_byte <> payload     
         end
         
     end
+
+    def int_to_binary(number) do
+    
+        number
+        |> Integer.digits(256) 
+        |> Enum.reduce(<<>>,
+            fn (number,binary) -> binary <> <<number>> end
+            )
+    end
+
+
+   
 end
--- test/encode_test.exs
+++ test/encode_test.exs
@@ -2,7 +2,9 @@ defmodule RLP.EncodeTest do
     use ExUnit.Case
     
     @table_tests [
-        {"dog",<<0x83,"d","o","g">>}
+        {"dog",<<0x83,"d","o","g">>},
+        {"this world has lot of good blockchain developers in them",
+        <<0xb8,56,"t", "h", "i", "s", " ", "w", "o", "r", "l", "d", " ", "h", "a", "s", " ", "l", "o", "t", " ", "o", "f", " ", "g", "o", "o", "d", " ", "b", "l", "o", "c", "k", "c", "h", "a", "i", "n", " ", "d", "e", "v", "e", "l", "o", "p", "e", "r", "s", " ", "i", "n", " ", "t", "h", "e", "m">>}
     ]
 
     test "encode an empty string" do
@@ -19,13 +21,16 @@ defmodule RLP.EncodeTest do
 
     test "encode different strings" do
         @table_tests
-        |> Enum.map(fn {payload,result} -> RLP.Encode.encode(payload) == result  end)
+        |> Enum.map(fn {payload,result} -> RLP.Encode.encode(payload) == result end)
         |> Enum.all?
         |> assert
     end
 
-    test "encode the set theoritical representation of 3" do
-        RLP.Encode.encode [[], [[]], [ [], [[]] ]]
+
+    test "integer to binary list conversion" do
+        assert RLP.Encode.int_to_binary(1024) == <<4,0>>
+        assert RLP.Encode.int_to_binary(1234) == <<4,210>>
+        assert RLP.Encode.int_to_binary(123411) == <<1, 226, 19>>
     end
 
     
